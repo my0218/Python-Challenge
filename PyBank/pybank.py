df0cee9b8da424e763ae0259c13dda60d91a1f36
my0218 <124230467+my0218@users.noreply.github.com>
2023-03-08 17:45:57 -0500

    Python Chanllenge

---
+++ PyBank/pybank.py
@@ -0,0 +1,57 @@
+import os
+import csv
+
+# Set up variables to store data
+
+rowcount = 0
+total_profit_losses = 0
+previous_month_profit = 0
+average_change = 0
+greatest_increase_month = ""
+greatest_increase = 0
+greatest_decrease_month = ""
+greatest_decrease = 0
+
+# Open the CSV file and read in the data
+with open('PyBank/Resources/budget_data.csv', newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    # skip header row
+    next(csvreader) 
+    for row in csvreader:
+        rowcount += 1
+        month = row[0]
+        profit_losses = int(row[1])
+        
+        # Calculate the total profit/losses
+        total_profit_losses += profit_losses
+        
+        # Calculate the change in profit from the previous month
+        change = profit_losses - previous_month_profit
+        
+        # Update the previous month's profit for the next iteration
+        previous_month_profit = profit_losses
+        
+        # Calculate the average change
+        if month != "Jan-10": # exclude first month from average calculation
+            average_change += change
+        
+        # Find the greatest increase and decrease in profits
+        if change > greatest_increase:
+            greatest_increase = change
+            greatest_increase_month = month
+        elif change < greatest_decrease:
+            greatest_decrease = change
+            greatest_decrease_month = month
+
+        # Calculate the average change (exclude first month)
+        average_change /= rowcount
+        
+
+# Output the results
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months:", rowcount)
+print(f"Total: ${total_profit_losses}")
+print(f"Average Change: ${average_change:.2f}")
+print(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})")
+print(f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})")
---
+++ PyPoll/pypoll.py
@@ -0,0 +1,43 @@
+import os
+import csv
+
+#list to store data
+rowcount = 0
+candidate1_votes = 0
+candidate2_votes = 0
+candidate3_votes = 0
+candidate1_pct = 0
+candidate2_pct = 0
+candidate3_pct = 0
+
+# Open the CSV file and read in the data
+with open('PyPoll/Resources/election_data.csv', newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    # skip header row
+    next(csvreader) 
+    #count number of votes
+    for row in csvreader:
+        rowcount += 1
+    #get votes of each candidates
+        if str(row[2]) == "Charles Casper Stockham":
+            candidate1_votes +=1
+        elif str(row[2]) == "Diana DeGette":
+            candidate2_votes +=1
+        else:
+            candidate3_votes +=1
+    # vote percentage of each candidates
+        candidate1_pct = round((candidate1_votes / rowcount) *100, 3)
+        candidate2_pct = round((candidate2_votes / rowcount) *100, 3)
+        candidate3_pct = round((candidate3_votes / rowcount) *100, 3)
+
+    result = {"Charles Casper Stockham":candidate1_votes, "Diana DeGette": candidate2_votes, "Raymon Anthony Doane": candidate3_votes}
+    winner = max (result, key=result.get)
+
+print ("Election Results")
+print ("---------------------")
+print ("Total Votes: ", rowcount)
+print ("---------------------")
+print ("Charles Casper Stockham: ", candidate1_pct,"% (", candidate1_votes, ")")
+print ("Diana DeGette: ", candidate2_pct, "% (", candidate2_votes, ")")
+print ("Raymon Anthony Doane: ", candidate3_pct, "% (", candidate3_votes, ")")
+print ("Winner: ", winner)
